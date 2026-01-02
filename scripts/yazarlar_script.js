5e3b14a534ba6dc3fa505d05fd67eba0777f0360
Onur Bolat <h4run0s3k41@gmail.com>
2026-01-02 21:40:42 +0300

    genel standartizasyon güncellemesi
    tüm dosyalar için css/semantik yapı güncellemesi

--- scripts/yazarlar_script.js
+++ scripts/yazarlar_script.js
@@ -4,14 +4,15 @@ const htmlElement = document.documentElement;
 
 const savedTheme = localStorage.getItem('theme') || 'light';
 htmlElement.setAttribute('data-theme', savedTheme);
-themeIcon.innerText = savedTheme === 'dark' ? '☀️' : '🌙';
+themeIcon.innerText = savedTheme === 'dark' ? '🌟' : '🌙';
 
 themeToggle.addEventListener('click', () => {
-    const currentTheme = htmlElement.getAttribute('data-theme');
-    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
-    htmlElement.setAttribute('data-theme', newTheme);
-    localStorage.setItem('theme', newTheme);
-    themeIcon.innerText = newTheme === 'dark' ? '☀️' : '🌙';
+const currentTheme = htmlElement.getAttribute('data-theme');
+const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+            
+htmlElement.setAttribute('data-theme', newTheme);
+localStorage.setItem('theme', newTheme);
+themeIcon.innerText = newTheme === 'dark' ? '🌟' : '🌙';
 });
 
 function switchAuthor(authorId, element) {
