2b818187d607f1c44548b1d0095da7669f602f03
Onur Bolat <h4run0s3k41@gmail.com>
2026-01-21 20:14:23 +0300

    svg + icon update

--- static/js/anasayfa_script.js
+++ static/js/anasayfa_script.js
@@ -3,17 +3,21 @@ const themeToggle = document.getElementById('theme-toggle');
 const themeIcon = document.getElementById('theme-icon');
 const htmlElement = document.documentElement;
 
+// Flask linklerini JS değişkenlerine aktaralım
+const sunIconPath = SUN_ICON;
+const moonIconPath = MOON_ICON;
+
 const savedTheme = localStorage.getItem('theme') || 'light';
 htmlElement.setAttribute('data-theme', savedTheme);
-themeIcon.innerText = savedTheme === 'dark' ? '☀️' : '🌙';
+
+themeIcon.src = savedTheme === 'dark' ? sunIconPath : moonIconPath;
 
 themeToggle.addEventListener('click', () => {
-const currentTheme = htmlElement.getAttribute('data-theme');
-const newTheme = currentTheme === 'light' ? 'dark' : 'light';
-            
-htmlElement.setAttribute('data-theme', newTheme);
-localStorage.setItem('theme', newTheme);
-themeIcon.innerText = newTheme === 'dark' ? '☀️' : '🌙';
+    const currentTheme = htmlElement.getAttribute('data-theme');
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+    htmlElement.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+    themeIcon.src = newTheme === 'dark' ? sunIconPath : moonIconPath;
 });
 
 
--- static/js/sablon_script.js
+++ static/js/sablon_script.js
@@ -1,19 +1,22 @@
- // tema değiştirme olayını diğer sayfalarla aynı yaptım bozulmasın diye
 const themeToggle = document.getElementById('theme-toggle');
 const themeIcon = document.getElementById('theme-icon');
 const htmlElement = document.documentElement;
 
+// Flask linklerini JS değişkenlerine aktaralım
+const sunIconPath = SUN_ICON;
+const moonIconPath = MOON_ICON;
+
 const savedTheme = localStorage.getItem('theme') || 'light';
 htmlElement.setAttribute('data-theme', savedTheme);
-themeIcon.innerText = savedTheme === 'dark' ? '☀️' : '🌙';
+
+themeIcon.src = savedTheme === 'dark' ? sunIconPath : moonIconPath;
 
 themeToggle.addEventListener('click', () => {
-const currentTheme = htmlElement.getAttribute('data-theme');
-const newTheme = currentTheme === 'light' ? 'dark' : 'light';
-            
-htmlElement.setAttribute('data-theme', newTheme);
-localStorage.setItem('theme', newTheme);
-themeIcon.innerText = newTheme === 'dark' ? '☀️' : '🌙';
+    const currentTheme = htmlElement.getAttribute('data-theme');
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+    htmlElement.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+    themeIcon.src = newTheme === 'dark' ? sunIconPath : moonIconPath;
 });
 
 
