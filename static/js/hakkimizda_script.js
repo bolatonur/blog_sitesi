ab0b6c85a52cd69218f994b18c9d131c51148c94
Onur Bolat <h4run0s3k41@gmail.com>
2026-02-03 00:42:02 +0300

    duzenle.html update
    duzenle kısmına medya ve kod blokları eklendi

--- static/js/hakkimizda_script.js
+++ static/js/hakkimizda_script.js
@@ -1,8 +1,9 @@
+
 const themeToggle = document.getElementById('theme-toggle');
 const themeIcon = document.getElementById('theme-icon');
 const htmlElement = document.documentElement;
 
-// Flask linklerini JS değişkenlerine aktaralım
+
 const sunIconPath = SUN_ICON;
 const moonIconPath = MOON_ICON;
 
@@ -20,22 +21,53 @@ themeToggle.addEventListener('click', () => {
 });
 
 let sonKaydirma = 0;
+let oncekiKontrolYeri = 0;
+let oncekiKontrolZamani = Date.now();
+
 const header = document.querySelector('.header');
+
+const hizLimiti = 1350; 
 
 window.addEventListener('scroll', function() {
     let suAnkiKaydirma = window.pageYOffset || document.documentElement.scrollTop;
+    let suAnkiZaman = Date.now();
 
     if (window.innerWidth <= 1300) {
-        if (suAnkiKaydirma > sonKaydirma && suAnkiKaydirma > 150) {
-            header.classList.add('gizli');
+
+        if (suAnkiKaydirma > sonKaydirma) {
+            if (suAnkiKaydirma > 150) {
+                header.classList.add('gizli');
+            }
+
+            oncekiKontrolYeri = suAnkiKaydirma;
+            oncekiKontrolZamani = suAnkiZaman;
         } 
-        // Sayfa en tepeye (0'a) çok yakınsa veya yukarı kaydırıyorsa her zaman göster
-        else if (suAnkiKaydirma < sonKaydirma || suAnkiKaydirma <= 50) {
-            header.classList.remove('gizli');
+        
+        else {
+            if (suAnkiKaydirma <= 50) {
+                 header.classList.remove('gizli');
+            }
+            else {
+                let zamanFarki = suAnkiZaman - oncekiKontrolZamani;
+
+
+                if (zamanFarki > 50) {
+                    let mesafe = oncekiKontrolYeri - suAnkiKaydirma; 
+                    let hiz = (mesafe / zamanFarki) * 1000; 
+
+                    if (hiz > hizLimiti) {
+                        header.classList.remove('gizli');
+                    }
+
+                    oncekiKontrolYeri = suAnkiKaydirma;
+                    oncekiKontrolZamani = suAnkiZaman;
+                }
+            }
         }
     } else {
         header.classList.remove('gizli');
     }
 
     sonKaydirma = suAnkiKaydirma <= 0 ? 0 : suAnkiKaydirma;
+
 }, false);
